17783b7e620bea7e4ff1c5424e859b8db77dee36
Kacper <scolioticstudios@gmail.com>
2025-11-18 20:14:19 +0100

    mostly completed navbar started on main page

--- src/MainHeader.vue
+++ src/MainHeader.vue
@@ -1,8 +1,10 @@
 <script setup>
+import Navbar from './Navbar.vue';
 </script>
 
 <template>
-    <div class="mx-auto my-0 py-0 bg-gray-800 w-96 h-16">
-        <h1 class="text-bold text-3xl text-orange-500 text-center p-2 mt-2">Witaj na forum dyskusyjnym</h1>
+    <Navbar />
+    <div class="mx-auto my-0 py-0 bg-gray-800 w-auto h-auto">
+        <h1 class="text-bold text-3xl text-orange-500 text-center p-2 mt-2">Witaj na stronie internetowej restauracji Self Made</h1>
     </div>
 </template>
--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,6 +1,35 @@
 <script setup>
+import {useRoute, useRouter, RouterLink} from 'vue-router'
+
+const isActiveLink = (routePath) => {
+  const route = useRoute();
+  return route.path === routePath;
+}
 </script>
 
 <template>
-    <nav class="bg-gray-800 grid grid-cols-3"></nav>
+    <nav class="bg-gray-800 border-b border-gray-900">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div class="flex h-20 items-center justify-between">
+                <div class="flex flex-1 items-right justify-right md:items-stretch md:justify-start">
+                    <div class="md:ml-auto">
+                        <div class="flex space-x-2">
+                            <RouterLink to="/" :class="[isActiveLink('/') ? 'bg-gray-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']">
+                                Strona Główna
+                            </RouterLink>
+                            <RouterLink to="/menu" :class="[isActiveLink('/menu') ? 'bg-gray-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']">
+                                Menu
+                            </RouterLink>
+                            <RouterLink to="/about" :class="[isActiveLink('/about') ? 'bg-gray-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']">
+                                Informacje
+                            </RouterLink>
+                            <RouterLink to="/pictures" :class="[isActiveLink('/pictures') ? 'bg-gray-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']">
+                                Zdjęcia
+                            </RouterLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
