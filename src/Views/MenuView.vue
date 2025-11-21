c2c92e89fd77a686fd4bc3d630b4ffe09df9125f
Kacper <scolioticstudios@gmail.com>
2025-11-21 23:25:14 +0100

    made better animation, color changes

--- src/Views/MenuView.vue
+++ src/Views/MenuView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import MenuItem from '@/components/MenuItem.vue'
 import axios from 'axios'
-import { onMounted, reactive, computed, watch} from 'vue';
+import { onMounted, reactive } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
@@ -10,19 +10,28 @@ const state = reactive({
     items: []
 });
 
+const toggleItemAnimation = (item) => {
+    item.isActive = !item.isActive;
+};
+
 const fetchItems = async () => {
     try {
         const response = await axios.get('http://localhost:5001/menuitems');
-        state.items = response.data;
+        
+        
+        
+        state.items = response.data.map(item => ({
+            ...item,
+            isActive: false 
+        }));
+        
     } catch (error) {
         console.error('Błąd wczytywania pozycji z menu:', error);
     }
 };
 
 onMounted(fetchItems);
-
 </script>
-
 
 <template>
     <img src="/public/pexels-efrem-efre-2786187-16124818.jpg" class="blur-sm flex object-cover bg-no-repeat absolute" />
@@ -32,11 +41,17 @@ onMounted(fetchItems);
             <div 
                 v-for="item in state.items" 
                 :key="item.id"
-                class="col-span-1 bg-gray-500/20 backdrop-blur-lg rounded-2xl mx-4 mt-2 h-64 transition delay-150 duration-300 ease-in-out hover:scale-200 hover:z-50"
-                :class="item.animation">
-                    <MenuItem :menuItem="item" ></MenuItem>
-                </div>
+                @click="toggleItemAnimation(item)"
+                class="col-span-1 bg-gray-500/20 backdrop-blur-lg rounded-2xl mx-4 mt-2 h-64 cursor-pointer z-30"
+                :class="{ [item.animation]: item.isActive }"
+                :style="{ 
+                    transition: item.isActive 
+                        ? 'all 300ms ease-in-out' 
+                        : 'all 300ms ease-in-out, z-index 0ms 300ms' 
+                }"
+            >
+            <MenuItem :menuItem="item"></MenuItem>
             </div>
-            
-        </div>
+        </div>    
+    </div>
 </template>
--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -11,7 +11,7 @@ const props = defineProps({
         <div class="ft col-span-1 text-center text-3xl text-gray-300">{{menuItem.name}}</div>
         <div class="ft col-span-1"></div> 
         <div class="ft col-span-2 text-center text-gray-300 text-lg">{{'Składniki: ' + menuItem.ingredients }}</div>
-        <div class="ft col-span-1 text-gray-300 text-sm text-shadow-sm text-center text-shadow-black"><i class="pi pi-clock" style="color:black; font-size:14px"></i>{{ 'Czas przygotowania: '+ menuItem.time + ' min'}}</div>
+        <div class="ft col-span-1 text-gray-300 text-sm text-shadow-sm text-center text-shadow-black"><i class="pi pi-clock" style="color:black; font-size:14px"></i>{{ ' Czas przygotowania: '+ menuItem.time + ' min'}}</div>
         <div class="ft col-span-1"></div>
         <div class="ft col-span-2 text-gray-300 text-sm text-shadow-sm text-center text-shadow-black">{{'Alergeny: ' + menuItem.allergens }}</div>
 
